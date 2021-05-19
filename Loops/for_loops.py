222caa0c3706dfac8607a6eee234420abf476060
Charles Lucas <charleslucas@Charles-MacBook-Pro.local>
2021-05-19 19:53:55 -0400

    Loops

---
+++ Loops/for_loops.py
@@ -0,0 +1,20 @@
+nums = [1, 2, 3, 4, 5];
+
+for num in nums:
+    print(num)
+
+# Goes From 1 to 9
+for num in range(1, 10):
+    print(num)
+
+# Goes from 0 to 9
+for num in range(10):
+    print(num)
+
+#Prints backwards from 7 to 1
+for num in range(7, 0, -1):
+    print(num)
+
+#Prints every odd number last number is the step
+for num in range(1, 10, 2):
+    print(num)
