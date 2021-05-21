4a70079662e4f276211f9d72ddf1275e8d0ea5fc
Charles <clucas0311@gmail.com>
2021-05-20 21:36:16 -0400

    Numbers

--- Loops/unlucky_numbers.py
+++ Loops/unlucky_numbers.py
@@ -1,7 +1,8 @@
+# Why isn't this working
 for num in range(1, 21):
     if num == 4 or num == 13:
         print(f"{num} is unlucky");
-    if num % 2 == 0:
+    elif num % 2 == 0:
         print(f"{num} is even")
-    if num % 2 != 0:
+    else:
         print(f"{num} is odd")
