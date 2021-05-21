ac58aa5b4a33624d783c72544e93c1df50220d86
Charles Lucas <charleslucas@charles-mbp.myfiosgateway.com>
2021-05-20 20:36:11 -0400

    numbers

--- Loops/unlucky_numbers.py
+++ Loops/unlucky_numbers.py
@@ -1,7 +1,7 @@
 for num in range(1, 21):
     if num == 4 or num == 13:
         print(f"{num} is unlucky");
-    elif num % 2 == 0:
+    if num % 2 == 0:
         print(f"{num} is even")
-    else:
+    if num % 2 != 0:
         print(f"{num} is odd")
